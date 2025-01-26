9afa884aaf7509fa2a5a93240097cce9e6ee1a6f
Mariana Kolesnichenko <mariana@anitechsolutions.com>
2025-01-27 00:09:46 +0200

    Added hello

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function sayHello(name, surname) {
   // New comment
-  console.log(`${name} ${surname}`); 
+  console.log(`Hewllo ${name} ${surname}`); 
 }
 // Comment for the branch 3
 sayHello('Mariana', 'Kolesnichenko');
